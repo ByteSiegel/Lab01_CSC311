cb233aac74a422a9cde8606f60c904cb125fd26a
Ben Siegel <siegelbenjamin03@gmail.com>
2024-08-29 13:26:30 -0400

    Add comments

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -11,14 +11,16 @@ private String brand;
         System.out.println("Honk, honk!");
     }
 
+    //prints the attributes of the Car including the inherited attributes from Vehicle
     public void displayInfo(){
-        System.out.println(getColor());
+        System.out.println(getColor());//this prints the getter from the parent class
         System.out.println(getEngineSize());
         System.out.println(getFuelType());
         System.out.println(getNumberOfWheels());
         System.out.println(getBrand());
     }
 
+    //getters and setters
     public String getBrand() {
         return brand;
     }
